ff449019a0703473c5e5e65184b26977d518a342
Jeff Salisbury <salisbury.jeffery@gmail.com>
2021-05-18 22:57:55 -0400

    Setting up layout.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <AppBar>
+      <v-main>
+        <h1>Test</h1>
+      </v-main>
+    </AppBar>
+  </v-app>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppBar from '@/components/AppBar'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    AppBar
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
